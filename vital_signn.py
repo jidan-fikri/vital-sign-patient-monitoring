cb7d83e62e125c11a5e918fb9c6e185da17c3b47
Jidan Fikri <68828496+jidan-fikri@users.noreply.github.com>
2022-08-02 20:14:44 +0700

    Adding revised version of vital_signn.py file
    - Merapihkan beberapa function
    - Memindahkan kode di perulangan while True ke masing-masing function

--- vital_signn.py
+++ vital_signn.py
@@ -1,45 +1,77 @@
 import time
 import serial
-ser=serial.Serial(port="COM7", baudrate=115200, bytesize=8, parity='N' )  
+import numpy as np
+ser=serial.Serial(port="COM", baudrate=115200, bytesize=8, parity='N' )  
 
+nibp = []
+nibp_temp = [] #menyimpan data sementara nibp dalam array
+spo2_temp = [] #menyimpan data sementara spo2 dalam array
 
-def wave():    
-    if (address_low == b'\00'):
-        address_high = ser.read()
-        #print(address_high)
-        if (address_high == b'\01'):
-            for i in range (0, bytecount_int):
-                EKGwave = ser.read()
-                EKGwave_int = int.from_bytes(EKGwave, "big")
-                #print(EKGwave_int)
-                
-        elif (address_high == b'\02'):
-            for i in range (0, bytecount_int):
-                SPO2wave = ser.read()
-                SPO2wave_int = int.from_bytes(SPO2wave, "big")
-                #print(SPO2wave_int)
+def wave(): 
+#Untuk menampilkan wave EKG dan SpO2
+    int_v = int.from_bytes(header, "big")
+    if (header == b'\02'):
+        bytecount = ser.read()
+        bytecount_str = str(bytecount)
+        if (bytecount != b'\ff'):
+            address_low = ser.read()
+            if (address_low == b'\00'):
+                address_high = ser.read()
+                if (address_high == b'\01'):
+                    for i in range (0, 3):
+                        EKGwave = ser.read()
+                        EKGwave_int = int.from_bytes(EKGwave, "big")
+                        print(EKGwave_int)                
+                elif (address_high == b'\02'):
+                    for i in range (0, 1):
+                        SPO2wave = ser.read()
+                        SPO2wave_int = int.from_bytes(SPO2wave, "big")
+                        print(SPO2wave_int)
           
 
 def num():
-    if (address_low == b'\01'):
-        address_high = ser.read()
-        # print(data3)
-        if (address_high == b'\01'):
-            for i in range (0, bytecount_int):
-                EKGnum = ser.read()
-                EKGnum_int = int.from_bytes(EKGnum, "big")
-                #print(EKGnum_int)
+#Untuk menampilkan nilai SpO2
+    int_v = int.from_bytes(header, "big")
+    if (header == b'\02'):
+        bytecount = ser.read()
+        bytecount_str = str(bytecount)
+        if (bytecount != b'\ff'):
+            address_low = ser.read()
+            if (address_low == b'\01'):
+                address_high = ser.read()
+                if (address_high == b'\02'):
+                    for i in range (0, 2):
+                        SPO2num = ser.read()
+                        SPO2num_int = int.from_bytes(SPO2num, "big")
+                        spo2_temp.append(SPO2num_int)
+                    result = float(f"{spo2_temp[-2]}.{spo2_temp[-1]}") #SpO2 data
+                    print(round(result, 2)) #Menampilkan nilai spo2 dengan pembulatan 2 angka dibelakang koma
 
-        elif (address_high == b'\02'):
-            #SPO2num_int= []
-            for i in range (0, bytecount_int):
-                SPO2num = ser.read()
-                SPO2num_int = int.from_bytes(SPO2num, "big")
-                #SPO2num_int.append (int.from_bytes(SPO2num, "big"))
-                #print(SPO2num_int)
 
-
-def NIB():
+def nibp_read ():
+#Untuk membaca tekanan darah dengan NIBP
+    if (header == b'\xA7'):
+            ya = ser.read()
+            if (ya == b'\x11'):
+                ye = ser.read()
+                if (ye == b'\x02'):
+                    for i in range (0,7):
+                        data = ser.read()
+                        data_int = int.from_bytes(data, "big")
+                        nibp_temp.append(data_int)                        
+                    nibp.append(nibp_temp)
+                    sys = nibp[0][0] #systolic data
+                    mapp = nibp[0][2] #map pressure data
+                    dias = nibp[0][4] #diastolic data
+                    pulse = nibp [0][6] #pulse/min data 
+                    print("Systholic pressure: ", sys)
+                    print("MAP Pressure: ", mapp)
+                    print("Diastolic Pressure: ", dias)
+                    print("Pulse/min: ", pulse)
+                    
+                    
+def NIBstart():
+#Untuk memulai pengukuran tekanan darah
     ser.write(b'\x02')
     ser.write(b'\xA3')
     ser.write(b'\x02')
@@ -51,62 +83,14 @@ def NIB():
     ser.write(b'\x03')
     
     
-NIB() 
+def NIBstop():
+
+
 
 
 while True:
         header = ser.read()
-        
-        if (header == b'\xA7'):
-            #print ("OKE")
-            ya = ser.read()
-            #print (ya)
-            if (ya == b'\x11'):
-                ye = ser.read()
-                #print (ye)
-                if (ye == b'\x02'):
-                    #yo = ser.read()
-                    for i in range (0,7):
-                        data = ser.read()
-                        data_int = int.from_bytes(data, "big")
-                        print (data_int)
-
-"""
-
-        int_v = int.from_bytes(header, "big")
-        if (header == b'\02'):
-            bytecount = ser.read()
-            bytecount_str = str(bytecount)
-            print (bytecount)
-            #bytecount_int = (int(bytecount_str[5]))
-            if (bytecount != b'\ff'):
-                address_low = ser.read()
-                #print(address_low)
-                if (address_low == b'\x11'):
-                    address_high = ser.read()
-                    #print(address_high)
-                    if (address_high == b'\x03'):
-                        for i in range (0, 7):
-                            inb = ser.read()
-                            inb_int = int.from_bytes(inb, "big")
-                            #print(inb_int)
-   """ 
-  
-
-#while True: 
-        #NIB()               
-        #header = ser.read()
-        #print (header)
-        #int_v = int.from_bytes(header, "big")
-        #if (header == b'\02'):
-           # bytecount = ser.read()
-            #bytecount_str = str(bytecount)
-            #bytecount_int = (int(bytecount_str[5]))
-           # if (bytecount != b'\ff'):
-               # address_low = ser.read()
-                #print(address_low)
-                #wave()
-                #num()
+        #drop function here
 
             
             
